bf4229880c52eea7b1a4bb93cd9970b1115a034e
Harijs <harijs@example.com>
2026-06-17 16:31:22 +0300

    Pievienoti aplikācijas maršruti: dashboard, priekšmetu un atzīmju CRUD, kalkulators

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/calculator', [DashboardController::class, 'calculator'])->name('calculator');
+
+Route::resource('subjects', SubjectController::class);
+
+Route::resource('grades', GradeController::class);
+
+Route::get('/grades/subject/{subject}', [GradeController::class, 'index'])->name('grades.subject');
